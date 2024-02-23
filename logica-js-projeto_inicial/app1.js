1b7a3227c1bcaf6d1cb561949df3673b459d5c6f
derPHell42 <theotherph@hotmail.com>
2024-02-23 13:18:35 -0300

    Criação de Jogo usando Html, CSS e Java Script

---
+++ logica-js-projeto_inicial/app1.js
@@ -0,0 +1,13 @@
+var numero = parseFloat(prompt("Digite um número:"));
+
+
+if (numero > 0) {
+    console.log("O número é positivo.");
+    alert("O número é positivo.");
+} else if (numero < 0) {
+    console.log("O número é negativo.");
+    alert("O número é negativo.");
+} else {
+    console.log("O número é zero.");
+    alert("O número é zero.");
+}
